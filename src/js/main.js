d6576ecbc5331aa97dbaa83ac8ef9d82779122dd
valikin <valikin@knoema.com>
2021-02-20 15:02:39 +0500

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
-let init = () => {
+var init = function () {
     if ($(window).width() <= 1023 && !$('.main .book').hasClass('mobile')) {
         $('.main .btn').before($('.main .book'));
         $('.main .book').addClass('mobile');
     }
     
     if ($(window).width() > 1023 && $('.main .book').hasClass('mobile')) {
-        console.log('object');
         $('.main .pdf').before($('.main .book'));
         $('.main .book').removeClass('mobile');
     }
@@ -14,8 +13,14 @@ let init = () => {
 init();
 $(window).resize(init);
 
-$('body').on('click', '.features a', () => {
+$('body').on('click', '.features a', function () {
     $("html, body").stop().animate({scrollTop: $(document).height()}, 500, 'swing', function() { 
-        // alert("Finished animating");
-     });
+        
+    });
+})
+
+$('body').on('click', '.btn', function () {
+    $("html, body").stop().animate({scrollTop: $(document).height()}, 500, 'swing', function() { 
+        
+    });
 })
