34ede69230ffcfec6efe1cd24ae9d5abac5cd834
valikin <valikin@knoema.com>
2021-02-20 13:29:03 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -1,9 +1,15 @@
-if ($(window).width() <= 1023 && !$('.main .book').hasClass('.mobile')) {
-    $('.main .btn').before($('.main .book'));
-    $('.main .book').addClass('mobile');
+let init = () => {
+    if ($(window).width() <= 1023 && !$('.main .book').hasClass('mobile')) {
+        $('.main .btn').before($('.main .book'));
+        $('.main .book').addClass('mobile');
+    }
+    
+    if ($(window).width() > 1023 && $('.main .book').hasClass('mobile')) {
+        console.log('object');
+        $('.main .pdf').before($('.main .book'));
+        $('.main .book').removeClass('mobile');
+    }
 }
 
-if ($(window).width() > 1023 && $('.main .book').hasClass('.mobile')) {
-    $('.main .pdf').before($('.main .book'));
-    $('.main .book').removeClass('mobile');
-}
+init();
+$(window).resize(init);
