3c952f83e210d1e36a5212214af4a2e5fb72bafe
valikin <valikin@knoema.com>
2021-02-20 15:24:01 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -1,26 +1,23 @@
-var init = function () {
-    if ($(window).width() <= 1023 && !$('.main .book').hasClass('mobile')) {
-        $('.main .btn').before($('.main .book'));
-        $('.main .book').addClass('mobile');
-    }
-    
-    if ($(window).width() > 1023 && $('.main .book').hasClass('mobile')) {
-        $('.main .pdf').before($('.main .book'));
-        $('.main .book').removeClass('mobile');
-    }
-}
+"use strict";
+
+var init = function init() {
+  if ($(window).width() <= 1023 && !$('.main .book').hasClass('mobile')) {
+    $('.main .btn').before($('.main .book'));
+    $('.main .book').addClass('mobile');
+  }
+
+  if ($(window).width() > 1023 && $('.main .book').hasClass('mobile')) {
+    console.log('object');
+    $('.main .pdf').before($('.main .book'));
+    $('.main .book').removeClass('mobile');
+  }
+};
 
 init();
 $(window).resize(init);
-
 $('body').on('click', '.features a', function () {
-    $("html, body").stop().animate({scrollTop: $(document).height()}, 500, 'swing', function() { 
-        
-    });
-})
-
-$('body').on('click', '.btn', function () {
-    $("html, body").stop().animate({scrollTop: $(document).height()}, 500, 'swing', function() { 
-        
-    });
-})
+  $("html, body").stop().animate({
+    scrollTop: $(document).height()
+  }, 500, 'swing', function () {// alert("Finished animating");
+  });
+});
